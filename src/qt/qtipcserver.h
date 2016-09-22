33312bc8eda79b968b02e9eab9864754f5f75601
pesobit <pesobit@gmail.com>
2016-09-21 20:48:36 -0500

    Pesobit Files
    Pesobit is a low-inflation Proof-of-Stake coin that was created to be
    used by Filipinos in Remittance, Freelancing, Marketplace and many more.

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Pesobit-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "PesobitURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
